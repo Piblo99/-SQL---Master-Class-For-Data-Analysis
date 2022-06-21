7a711c8bdd8050b7a4a08ead9f524a37787f7173
Paul Hankins <42610498+Piblo99@users.noreply.github.com>
2022-06-21 15:45:39 -0500

    Update Web Analysis project.sql

--- Web Analysis project.sql
+++ Web Analysis project.sql
@@ -1,114 +1,87 @@
--- 1:	Pageviews vs bounce rate by day
+-- 1:  Pageviews vs bounce rate by day
+SELECT pa.datekey,
+       Sum(pa.[pageviews])   AS [PageViews],
+       Avg(pa.bounceratepct) AS AvgBounceRatePct
+FROM   [dbo].[pageanalysis] pa
+       INNER JOIN [dbo].[calendar] cal
+               ON pa.datekey = cal.datekey
+GROUP  BY pa.datekey
+ORDER  BY pa.datekey
 
-select
-	 pa.DateKey
-	,sum(pa.[PageViews]) as [PageViews] 
-	,avg(pa.BounceRatePct) as AvgBounceRatePct
-from
-	[dbo].[PageAnalysis] pa inner join
-	[dbo].[Calendar] cal on pa.DateKey = cal.DateKey
-group by
-	 pa.DateKey
-order by 
-	 pa.DateKey
-
--- 2:	Pageviews vs bounce by week
-
-select
-	 cal.YearNum
-	,cal.WeekNumYear
-	,sum(pa.[PageViews]) as [PageViews] 
-	,avg(pa.BounceRatePct) as AvgBounceRatePct
-from
-	[dbo].[PageAnalysis] pa inner join
-	[dbo].[Calendar] cal on pa.DateKey = cal.DateKey
-group by
-	 cal.YearNum
-	,cal.WeekNumYear
-order by 
-	 cal.YearNum
-	,cal.WeekNumYear
+-- 2:  Pageviews vs bounce by week
+SELECT cal.yearnum,
+       cal.weeknumyear,
+       Sum(pa.[pageviews])   AS [PageViews],
+       Avg(pa.bounceratepct) AS AvgBounceRatePct
+FROM   [dbo].[pageanalysis] pa
+       INNER JOIN [dbo].[calendar] cal
+               ON pa.datekey = cal.datekey
+GROUP  BY cal.yearnum,
+          cal.weeknumyear
+ORDER  BY cal.yearnum,
+          cal.weeknumyear
 
 --  3:  Pageviews vs bounce rate by month
+SELECT cal.[monthyearname],
+       Sum(pa.[pageviews])   AS [PageViews],
+       Avg(pa.bounceratepct) AS AvgBounceRatePct
+FROM   [dbo].[pageanalysis] pa
+       INNER JOIN [dbo].[calendar] cal
+               ON pa.datekey = cal.datekey
+GROUP  BY cal.[monthyearname]
+ORDER  BY cal.[monthyearname]
 
-select
-	 cal.[MonthYearName]
-	,sum(pa.[PageViews]) as [PageViews] 
-	,avg(pa.BounceRatePct) as AvgBounceRatePct
-from
-	[dbo].[PageAnalysis] pa inner join
-	[dbo].[Calendar] cal on pa.DateKey = cal.DateKey
-group by
-	cal.[MonthYearName]
-order by 
-	cal.[MonthYearName]
-		
--- 4:	Avg session duration New visitor by week
+-- 4:  Avg session duration New visitor by week
+SELECT Year([datekey])                                          AS YearNum,
+       Datepart(week, [datekey])                                AS WeekNum,
+       Avg(Datediff(millisecond, 0, avgsessionduration)) / 1000 AS
+       AvgSessionDuration,
+       Count(va.[usertypekey])                                  AS
+       WeeklyNewVisitor
+FROM   [dbo].[visitoranalysis] va
+       INNER JOIN [dbo].[usertype] ut
+               ON va.usertypekey = ut.usertypekey
+                  AND ut.usertype = 'New Visitor'
+GROUP  BY Year([datekey]),
+          Datepart(week, [datekey])
+ORDER  BY Year([datekey]),
+          Datepart(week, [datekey])
 
-select
-	 year([DateKey]) as YearNum
-	,datepart(week,[DateKey]) as WeekNum
-	,avg(datediff(millisecond,0,AvgSessionDuration))/1000 as  AvgSessionDuration
-	,count(va.[UserTypeKey]) as WeeklyNewVisitor
-from
-	[dbo].[VisitorAnalysis] va inner join
-	[dbo].[UserType] ut on va.UserTypeKey = ut.UserTypeKey and
-                                            ut.UserType = 'New Visitor'
-group by
-	 year([DateKey]) 
-	,datepart(week,[DateKey]) 
-order by 
-	 year([DateKey]) 
-	,datepart(week,[DateKey]) 
+-- 4a:  Avg session duration Returning visitor by week  
+SELECT Year([datekey])                                          AS YearNum,
+       Datepart(week, [datekey])                                AS WeekNum,
+       Avg(Datediff(millisecond, 0, avgsessionduration)) / 1000 AS
+       AvgSessionDuration
+       -- Use Datediff here to calculate the seconds as we cannot just use numeri functions on time data types
+       ,
+       Count(va.[usertypekey]) AS WeeklyReturningVisitor
+FROM   [dbo].[visitoranalysis] va
+       INNER JOIN [dbo].[usertype] ut
+               ON va.usertypekey = ut.usertypekey
+                  AND ut.usertype = 'Returning Visitor'
+GROUP  BY Year([datekey]),
+          Datepart(week, [datekey])
+ORDER  BY Year([datekey]),
+          Datepart(week, [datekey])
 
--- 4a:	Avg session duration Returning visitor by week	
+-- 5:  Sessions vs avg Pages/Session by week
+SELECT Year([datekey]) AS YearNum,
+       Datepart(week, [datekey]) AS WeekNum,
+       Sum([sessions]) AS Sessions,
+       Cast(Avg([pagessession]) AS DECIMAL(18, 2)) AS AvgPagesSession
+FROM   [dbo].[visitoranalysis] va
+GROUP  BY Year([datekey]),
+          Datepart(week, [datekey])
+ORDER  BY Year([datekey]),
+          Datepart(week, [datekey])
 
-	select
-		 year([DateKey]) as YearNum
-		,datepart(week,[DateKey]) as WeekNum
-		,avg(datediff(millisecond,0,AvgSessionDuration))/1000 as  AvgSessionDuration		-- Use Datediff here to calculate the seconds as we cannot just use numeri functions on time data types
-		,count(va.[UserTypeKey]) as WeeklyReturningVisitor
-	from
-		[dbo].[VisitorAnalysis] va inner join
-		[dbo].[UserType] ut on va.UserTypeKey = ut.UserTypeKey and
-                                                ut.UserType = 'Returning Visitor'
-	group by
-		 year([DateKey]) 
-		,datepart(week,[DateKey]) 
-	order by 
-		 year([DateKey]) 
-		,datepart(week,[DateKey]) 
-
-
--- 5:	Sessions vs avg Pages/Session by week
-
-select
-	 year([DateKey]) as YearNum
-	,datepart(week,[DateKey]) as WeekNum
-	,sum([Sessions]) as Sessions
-	,cast(avg([PagesSession]) as decimal(18,2)) as AvgPagesSession
-from
-	[dbo].[VisitorAnalysis] va
-group by
-	 year([DateKey]) 
-	,datepart(week,[DateKey])
-order by 
-	 year([DateKey]) 
-	,datepart(week,[DateKey])
-
--- 6:	New users vs Pageviews by week
-
-select
-	 year(DateKey) as YearNum	
-	,datepart(week,[DateKey]) as WeekNum
-	,sum([NewUsers]) as NewUsers
-	,sum([PageViews]) as PageViews
-from
-	[dbo].[PageAnalysis]
-group by
-	 year([DateKey]) 
-	,datepart(week,[DateKey]) 
-order by 
-	 year([DateKey]) 
-	,datepart(week,[DateKey]) 
-
+-- 6:  New users vs Pageviews by week
+SELECT Year(datekey) AS YearNum,
+       Datepart(week, [datekey]) AS WeekNum,
+       Sum([newusers]) AS NewUsers,
+       Sum([pageviews]) AS PageViews
+FROM   [dbo].[pageanalysis]
+GROUP  BY Year([datekey]),
+          Datepart(week, [datekey])
+ORDER  BY Year([datekey]),
+          Datepart(week, [datekey]) 
